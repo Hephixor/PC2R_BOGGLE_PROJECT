e383ae759d54411901960d601bfbb4c226005e09
Hephixor <wsergeant.t@gmail.com>
2018-05-05 19:43:03 +0200

    [Clean]

---
+++ client/src/main/client/chatwindow/CellRenderer.java
@@ -0,0 +1,55 @@
+package main.client.chatwindow;
+
+
+import java.io.File;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.util.Callback;
+import main.messages.User;
+
+/**
+ * A Class for Rendering users images / name on the userlist.
+ */
+class CellRenderer implements Callback<ListView<User>,ListCell<User>>{
+        @Override
+    public ListCell<User> call(ListView<User> p) {
+
+        ListCell<User> cell = new ListCell<User>(){
+
+            @Override
+            protected void updateItem(User user, boolean bln) {
+                super.updateItem(user, bln);
+                setGraphic(null);
+                setText(null);
+                if (user != null) {
+                    HBox hBox = new HBox();
+
+                    Text name = new Text(user.getName());
+
+                    ImageView statusImageView = new ImageView();
+                    File stsimg = new File("/home/skylab/UPMC/M1S2/PC2R/PC2R_BOGGLE_PROJECT/client/src/main/resources/images/"+ user.getStatus().toString().toLowerCase() + ".png");
+                    Image statusImage = new Image(stsimg.toURI().toString(), 16, 16,true,true);
+                    statusImageView.setImage(statusImage);
+
+                    ImageView pictureImageView = new ImageView();
+                    File pimg = new File("/home/skylab/UPMC/M1S2/PC2R/PC2R_BOGGLE_PROJECT/client/src/main/resources/images/"+ user.getPicture().toLowerCase() + ".png");
+                    
+                    Image image = new Image(pimg.toURI().toString(),50,50,true,true);
+                    pictureImageView.setImage(image);
+
+                    hBox.getChildren().addAll(statusImageView, pictureImageView, name);
+                    hBox.setAlignment(Pos.CENTER_LEFT);
+
+                    setGraphic(hBox);
+                }
+            }
+        };
+        return cell;
+    }
+}
---
+++ client/src/main/client/login/MainLauncher.java
@@ -0,0 +1,59 @@
+package main.client.login;
+
+import java.io.File;
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class MainLauncher extends Application {
+
+    private static Stage primaryStageObj;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    	
+    	VBox vbox = new VBox();
+		vbox.setPadding(new Insets(10));
+		vbox.setSpacing(8);
+
+		Text title = new Text("Data");
+		title.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+		vbox.getChildren().add(title);
+    	
+        primaryStageObj = primaryStage;
+        URL fxml = new File("/home/skylab/UPMC/M1S2/PC2R/PC2R_BOGGLE_PROJECT/client/src/main/resources/views/LoginView.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(fxml);
+        
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setTitle("Boggle is life");
+//        File img = new File("/home/skylab/UPMC/M1S2/PC2R/PC2R_BOGGLE_PROJECT/client/src/main/resources/images/plug.png");
+//        primaryStage.getIcons().add(new Image(img.toURI().toString()));
+        Scene mainScene = new Scene(root, 350, 420);
+        mainScene.setRoot(root);
+        primaryStage.setResizable(false);
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+        primaryStage.setOnCloseRequest(e -> Platform.exit());
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public static Stage getPrimaryStage() {
+        return primaryStageObj;
+    }
+}
